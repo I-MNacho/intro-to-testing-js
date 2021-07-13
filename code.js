1152c0e39e0622cdf989b1e69312b5987280bbf8
Ignacio Mares <ignacio.mares00@gmail.com>
2021-07-13 15:20:47 -0500

    added another test that returns hello alex

--- code.js
+++ code.js
@@ -3,6 +3,8 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello(){
-    return 'Hello, Jane!';
+function sayHello(input){
+    if (input === "Alex"){
+        return "Hello, Alex!"
+    } else return "Hello, Jane!";
 }
